3e232c7f70a217e70a749567029a561f1541790e
LIUCBK <liucbk@bc.edu>
2025-02-08 13:02:49 -0500

    Update after slider control and decimal formatting lesson

--- TwoWayBindings/ContentView.swift
+++ TwoWayBindings/ContentView.swift
@@ -8,13 +8,57 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var count = 0
+    @State private var toggleIsOn = true
+    @State private var name = ""
+    @State private var selectedColor: Color = .red
+    @State private var selectedDate = Date()
+    @State private var stepperValue = 1
+    @State private var sliderValue = 50.0
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Spacer()
+            
+            Text("Press conut: \(count)")
+                
+            Button("Press Me"){
+                count += 1
+            }
+            .buttonStyle(.borderedProminent)
+            
+            Spacer()
+            
+            TextField("Enter a name", text: $name)
+                .textFieldStyle(.roundedBorder)
+            Text("Name entered: \(name)")
+            
+            Spacer()
+            
+            Toggle("Toggle is \(toggleIsOn ? "on" : "off"):", isOn: $toggleIsOn)
+            
+            Spacer()
+            
+            Rectangle()
+                .fill(selectedColor)
+                .frame(width: 100, height: 100)
+            
+            ColorPicker("Pick a Rectangle color:", selection: $selectedColor)
+            
+            Spacer()
+            
+            DatePicker("Date:", selection: $selectedDate)
+            Text("Selected date is: \(selectedDate.formatted(date: .abbreviated, time: .shortened))")
+            
+            Spacer()
+            
+            Stepper("Stepper valu: \(stepperValue)", value: $stepperValue, in: 1...10)
+            
+            Spacer()
+            
+            Slider(value: $sliderValue, in: 0...100)
+            Text("Slider value: \(Int(sliderValue))")
         }
+        .font(.title2)
         .padding()
     }
 }
