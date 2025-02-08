3daaab5b68b4f3aafcbea9f416310c091c09179f
LIUCBK <liucbk@bc.edu>
2025-02-08 12:24:19 -0500

    Initial Commit

---
+++ TwoWayBindings/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TwoWayBindings
+//
+//  Created by 刘佳雨 on 2025/2/8.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TwoWayBindings/TwoWayBindingsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwoWayBindingsApp.swift
+//  TwoWayBindings
+//
+//  Created by 刘佳雨 on 2025/2/8.
+//
+
+import SwiftUI
+
+@main
+struct TwoWayBindingsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
